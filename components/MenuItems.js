a8a49480d8315d8a9980095b1af2bac3f7c8c167
MarinaCouso <marina.couso.martinez@gmail.com>
2021-03-06 10:40:44 +0100

    fix touchable opacity child

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MenuTitle } from './TextComponents';
 
 const MenuItem = ({ text }) => {
-  return <MenuTitle text={text} />;
+  return <MenuTitle>{text}</MenuTitle>;
 };
 
 export { MenuItem };
